a48bc44df9f11a41fa0f7284bd03a28d22c154c9
Siddhi2325 <siddhihon25@gmail.com>
2025-04-24 11:33:26 +0530

    Update JCICD.java

--- JCICD.java
+++ JCICD.java
@@ -3,5 +3,8 @@ public static void main(String args[]){
   for(int i=0;i<=10;i++){
 System.out.println("Hello All this is Jay_CICD");
   }
+  for(int i=0;i<=10;i++){
+System.out.println("Hello All this is Siddhi_CICD");
+  }
   }
 }
